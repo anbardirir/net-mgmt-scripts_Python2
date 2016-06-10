b8f1235b748995282fdb30c47b90ba25ee1b128b
Todd Riemenschneider <twr14152@gmail.com>
2016-06-10 11:02:26 -0400

    Create eapi_script_interactive.py

---
+++ eapi_script_interactive.py
@@ -0,0 +1,38 @@
+Enter fil#!/usr/bin/python
+
+
+# The goal of this script was to try and make use of eapi with out having to create host and cli_command file
+# Keeping the script interactive
+# Needless to say its buggy and eapi does not accept partial command syntax
+# Not real functional
+# Keeping this as a work in progress
+
+from jsonrpclib import Server
+
+UN = "<>"
+PW = "<>"
+EPW = "<>"
+
+device_list = []
+while (True):
+    units = raw_input('Enter devices one at a time hit enter after each, type exit when done:\n ')
+    if units == 'exit': break
+    device_list.append(units)
+
+print device_list
+
+host_commands = [{ "cmd": "enable", "input": EPW}]
+
+host_commands = []
+while (True):
+    host_command = raw_input('Enter configuration commands one line at a time, type exit when done:\n ')
+    if host_command == 'exit': break
+    host_commands.append(host_command)
+print host_commands
+
+for ip in device_list:
+    switch = Server ("http://%s:%s@%s/command-api" % (UN, PW, ip))
+    response = switch.runCmds( 1,host_commands, 'json')
+    print response
+
+
