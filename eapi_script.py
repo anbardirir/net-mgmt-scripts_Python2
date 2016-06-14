c2d68a298639da14eae7ee2089f9743496d99954
Todd Riemenschneider <twr14152@gmail.com>
2016-06-14 12:22:11 -0400

    Update eapi_script.py

--- eapi_script.py
+++ eapi_script.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python
 
 from jsonrpclib import Server
+from pprint import pprint
 import getpass
-#
-# I added these authentication commands so I wouldnt have to store passwords in the file.
-# I also think it makes one more deliberate in their application as well as may provide some safe guards
-# from inadvertantly kicking the script off
-# I think it would be useful in producation. In lab testing is kind sucks!!.
-# To work around entering passwords all the time
-# comment out the current variables UN, PW, EPW, add them as static variable assignments
-# UN = "password"
-# PW = "password2"
-# EPW = "password3"
-#
+
+# 
+# I added these authentication commands so I wouldnt have to store passwords in the file. 
+# I also think it makes one more deliberate in their application as well as may provide some safe guards 
+# from inadvertantly kicking the script off 
+# I think it would be useful in producation. In lab testing is kind sucks!!. 
+# To work around entering passwords all the time 
+# comment out the current variables UN, PW, EPW, add them as static variable assignments 
+# UN = "user1" 
+# PW = "password2" 
+# EPW = "password3" 
+
 UN = raw_input("Username : ")
 PW = getpass.getpass("Password : ")
 EPW = getpass.getpass("enable password: ")
-
 
 device_list = []
 #You will need to create a file device_list.txt that will have the host you want to access
@@ -42,5 +43,5 @@ print host_commands
 for ip in device_list:
     switch = Server ("http://%s:%s@%s/command-api" % (UN, PW, ip))
     response = switch.runCmds( 1,host_commands, 'json')
-    print response
+    pprint(response)
 
