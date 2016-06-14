e756fc690f49533dfd25c541d6050fc5708b843c
Todd Riemenschneider <twr14152@gmail.com>
2016-06-14 08:11:04 -0400

    Update eapi_script_interactive.py

--- eapi_script_interactive.py
+++ eapi_script_interactive.py
@@ -29,7 +29,7 @@ print device_list
 
 host_commands = [{ "cmd": "enable", "input": EPW}]
 while (True):
-    host_command = raw_input('Enter commands one line at a time. Hit enter after each device. Type exit when done.:\n ')
+    host_command = raw_input('Enter commands one line at a time. Hit enter after each command line. Type exit when done.:\n ')
     if host_command == 'exit': break
     host_commands.append(host_command)
 
