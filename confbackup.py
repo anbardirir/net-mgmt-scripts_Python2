880ab3a55974716203988eb1c212d1563cedee61
Todd Riemenschneider <twr14152@gmail.com>
2017-06-27 12:32:32 -0400

    Create confbackup.py

---
+++ confbackup.py
@@ -0,0 +1,34 @@
+
+import getpass
+import telnetlib
+
+# Get Username and Password
+user = raw_input("Enter your username: ")
+password = getpass.getpass()
+
+#  File with list of devices
+f = open ("host.txt")
+
+#  Telnet to each device and cofigure it
+for line in f:
+	print "Getting running-config " + (line)
+	HOST = line.strip()
+	tn = telnetlib.Telnet(HOST)
+
+	tn.read_until("Username: ")
+	tn.write(user + "\n")
+	if password:
+	    tn.read_until("Password: ")
+	    tn.write(password + "\n")
+        tn.write("enable\n")
+        tn.write(password +"\n")
+	tn.write("terminal length 0\n")
+	tn.write("show run\n")
+	tn.write("exit\n")
+
+    readoutput = tn.read_all()
+    saveoutput =  open("router_" + HOST, "w")
+    saveoutput.write(readoutput)
+    saveoutput.write("\n")
+	saveoutput.close
+    print tn.read_all()
