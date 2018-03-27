c900b82cc88563becbe153be0ee4414e580451f0
Todd Riemenschneider <twr14152@gmail.com>
2018-03-27 10:10:18 -0400

    telnet script to gather info and save in file

---
+++ telnet_script_py2.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import getpass
+import telnetlib
+user = raw_input("Enter your username: ")
+password = getpass.getpass()
+
+host = raw_input("Enter hostnames or IPs seperated by space : ")
+cmd = raw_input("Enter commands serperated with ',': ")
+
+#Create lists to iterate through
+hosts = host.split()
+cmds = cmd.split(",")
+
+for HOST in hosts:
+    print("Connecting to host:",  HOST)
+    tn = telnetlib.Telnet(HOST)
+    tn.read_until("Username: ")
+    tn.write(user + "\n")
+    if password:
+        tn.read_until("Password: ")
+	tn.write(password + "\n")
+        tn.write("enable\n")
+        tn.write(password +"\n")
+	tn.write("terminal length 0\n")
+        for CMD in cmds:
+            tn.write(CMD + "\n")
+	tn.write("exit\n")
+
+    readoutput = tn.read_all()
+    saveoutput =  open("router_" + HOST, "w")
+    saveoutput.write(readoutput)
+    saveoutput.write("\n")
+    saveoutput.close
+print(tn.read_all())
